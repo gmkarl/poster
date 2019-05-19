16f2067b1d4b5318c20d9bf0da3ccb04b8e8ace0
Karl Semich <gmkarl@gmail.com>
2019-05-19 18:11:41 -0400

    -

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+#!/usr/bin/env node
 var fs = require('fs');
+
+// This code is messy because it is produced to handle a brain implant's group cognitive effects.
+// The developer is, hence, incredibly confused by the situation's attempt to inhibit him out of fear of being discovered.
 
 // we need to work, posting is likely an error
 // (it'll be cleaned up by somebody in a different state of mind)
@@ -9,9 +13,16 @@ var fs = require('fs');
 // - [ ] 2. run
 // - [ ] 3. check behavior
 
-fs.readFile('bitcoincash.key', 'utf8', function(err, contents) {
-	require('datacash').send({
-		data: ["0x6d02", "MESSAGE"],
-		cash: { key: contents }
+fs.readFile('message.1', 'utf8', function(err, contents) {
+	post(contents);
 });
+
+function post(msg) {
+	fs.readFile('bitcoincash.key', 'utf8', function(err, contents) {
+		require('datacash').send({
+			data: ["0x6d02", msg],
+			cash: { key: contents }
+		});
+	});
+}
 
