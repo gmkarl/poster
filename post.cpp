0c819de549872e67c3b3976ced11705d16c881dd
Karl Semich <gmkarl@gmail.com>
2019-05-19 17:04:38 -0400

    struggle

--- post.cpp
+++ post.cpp
@@ -2,6 +2,13 @@
 
 int main(int, char**)
 {
+	// https://memo.cash/profile/1LDRWEGhBuoweM5B18Av2oSTeh3Gjb64Vr?p=posts-and-replies
+
+	// when doing this you lose most thoughts,
+	// only the ones BOTH practiced through insanity and supported by the people supporting you
+
+	// please note it will take a long time to finish this, possibly hours, slightly possibly days
+	// i am alone and safe, and working with nobody else
 	CURL *curl;
 	
 	curl = curl_easy_init();
