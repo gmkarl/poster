123e05f456878e983d598d782306667dbd2b78d9
Karl Semich <gmkarl@gmail.com>
2019-05-19 17:14:07 -0400

    switch to shell

---
+++ post.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+########## https://memo.cash/profile/1LDRWEGhBuoweM5B18Av2oSTeh3Gjb64Vr?p=posts-and-replies
+
+########## when doing this you lose most thoughts and hearing,
+########## only the ones BOTH practiced through insanity and supported by the people supporting you
+
+########## please note it will take a long time to finish this, possibly hours, slightly possibly days
+########## i am alone and safe, and working with nobody else
+
+# post message
+
